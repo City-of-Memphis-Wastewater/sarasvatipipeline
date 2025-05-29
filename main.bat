95d2a9bb9f3c02ffa1d18088757d08154d8372fb
George Clayton Bennett <GEORGE.BENNETT@memphistn.gov>
2025-05-29 16:25:52 -0500

    lazarus

--- main.bat
+++ main.bat
@@ -1,6 +1,8 @@
 #@echo off
 #"---- $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') ----" >> daemon_log.txt
-poetry run python -c "from datetime import datetime; print('----', datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '----')" >> daemon_log.txt
+#poetry run python -c "from datetime import datetime; print('----', datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '----')" >> daemon_log.txt
+poetry run python -c "from datetime import datetime; print('----', datetime.now().strftime('%%Y-%%m-%%d %%H:%%M:%%S'), '----')" >> daemon_log.txt
+
 #where poetry
 poetry --version
 poetry run python -m projects.eds_to_rjn.scripts.daemon_runner >> daemon_log.txt 2>&1
