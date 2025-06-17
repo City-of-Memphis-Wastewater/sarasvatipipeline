ac7d528a570c48820ef57e9450347ae6257d62bb
George Clayton Bennett <GEORGE.BENNETT@memphistn.gov>
2025-06-17 18:14:30 -0500

    when shall we round the time stamps?

--- projects/eds_to_rjn/scripts/daemon_runner.py
+++ projects/eds_to_rjn/scripts/daemon_runner.py
@@ -2,7 +2,7 @@
 import schedule, time
 #import logging
 import datetime
-from ..scripts import collector, storage, aggregator, sanitizer
+#from ..scripts import collector, storage, aggregator, sanitizer
 from ..code import collector, storage, aggregator, sanitizer
 from src.pipeline.api.eds import login_to_session, get_query_point_list # actually generalized beyond EDS
 from .main import get_eds_maxson_token_and_headers, get_rjn_tokens_and_headers
